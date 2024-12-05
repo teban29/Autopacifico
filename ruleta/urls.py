95704a404cfa8ae5a7bec6b19e1cdcf196a376b8
Esteban Cardona Gonzalez <aprendizsistemas@valley.loc>
2024-12-05 17:29:01 -0500

    Ajustes

--- ruleta/urls.py
+++ ruleta/urls.py
@@ -3,30 +3,35 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    # Registro y ruleta
     path('registro/', views.RegistroClienteView.as_view(), name='registro_cliente'),
-    path('jugar/<int:cliente_id>/', views.jugar_ruleta, name='jugar_ruleta'),  # Jugar con cliente_id
-    path('jugar/', views.jugar_ruleta, name='jugar_ruleta'),  # Jugar sin cliente_id
+    path('jugar/', views.jugar_ruleta, name='jugar_ruleta'),  # Maneja cliente_id desde la sesión
+
+    # Guardar resultado
     path('guardar_resultado/', views.guardar_resultado, name='guardar_resultado'),
     
-    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),  # Ruta para logout
+    # Logout
+    path('admin/login/', views.AdminLoginView.as_view(), name='admin_login'),
 
-    # Rutas del CRUD para admin
-    
-    #CRUD premios
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+
+    # CRUD premios
     path('admin/premios/', views.PremiosCRUDView.as_view(), name='premios_crud'),
     path('admin/premios/editar/<int:pk>/', views.EditarPremioView.as_view(), name='editar_premio'),
     path('admin/premios/crear/', views.CrearPremioView.as_view(), name='crear_premio'),
     path('admin/premios/eliminar/<int:pk>/', views.EliminarPremioView.as_view(), name='eliminar_premio'),
     path('premios/toggle/<int:pk>/', views.toggle_estado_premio, name='toggle_estado_premio'),
-    
-    
-    # CRUD de clientes
+
+    # CRUD clientes
     path('admin/clientes/', views.ClientesListView.as_view(), name='clientes_crud'),
     path('admin/clientes/crear/', views.CrearClienteView.as_view(), name='crear_cliente'),
     path('admin/clientes/editar/<int:pk>/', views.EditarClienteView.as_view(), name='editar_cliente'),
     path('admin/clientes/eliminar/<int:pk>/', views.EliminarClienteView.as_view(), name='eliminar_cliente'),
 
+    # CRUD ganadores
     path('admin/ganadores/', views.GanadoresListView.as_view(), name='ganadores_crud'),
     path('admin/ganadores/eliminar/<int:pk>/', views.EliminarGanadorView.as_view(), name='eliminar_ganador'),
+
+    # Dashboard
     path('dashboard/', views.dashboard, name='dashboard'),
 ]
--- ruleta/views.py
+++ ruleta/views.py
@@ -8,21 +8,43 @@ from django.utils.timezone import now
 from django.views.decorators.csrf import csrf_exempt
 import json
 from django.http import JsonResponse
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.shortcuts import get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.mixins import UserPassesTestMixin
-from django.contrib.auth.decorators import user_passes_test
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
-from django.contrib.auth.decorators import user_passes_test
 from django.urls import reverse_lazy, reverse
 from django.views.decorators.http import require_POST
-from django.views.decorators.csrf import csrf_exempt
 from collections import Counter
 from django.db.models import Count
 from django.db.models.functions import TruncDay
+from django.contrib.auth import login, logout
 
 
+class AdminLoginView(View):
+    def get(self, request):
+        return render(request, 'login.html')
+
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            if user.is_superuser:  # Verificar si es un administrador
+                login(request, user)
+                return redirect('dashboard')  # Redirigir al dashboard del administrador
+            else:
+                messages.error(request, 'No tienes permisos para acceder a esta sección.')
+        else:
+            messages.error(request, 'Usuario o contraseña incorrectos.')
+
+
+        return render(request, 'login.html')
+
+
+
+# Registro de clientes
 class RegistroClienteView(View):
     def get(self, request):
         form = ClienteForm()
@@ -33,21 +55,21 @@ class RegistroClienteView(View):
         if form.is_valid():
             cliente = form.save()
             request.session['cliente_id'] = cliente.id
-            return redirect('jugar_ruleta', cliente_id=cliente.id)
+            return redirect('jugar_ruleta')
         else:
             messages.error(request, 'Error al registrar cliente. Verifica los datos ingresados.')
         return render(request, 'ruleta/registro_cliente.html', {'form': form})
 
 
-
+# Obtener premios
 @login_required
 def obtener_premios(request):
     premios = list(Premio.objects.filter(estado=True).values('nombre', 'probabilidad'))
     return JsonResponse({'premios': premios})
 
-@login_required(login_url='/ruleta/registro/')
-def jugar_ruleta(request, cliente_id=None):
-    cliente_id = cliente_id or request.session.get('cliente_id')
+# Jugar a la ruleta
+def jugar_ruleta(request):
+    cliente_id = request.session.get('cliente_id')
     if not cliente_id:
         messages.error(request, 'Por favor, regístrate para jugar.')
         return redirect('registro_cliente')
@@ -75,7 +97,7 @@ def jugar_ruleta(request, cliente_id=None):
     return render(request, 'ruleta/jugar.html', {'cliente': cliente, 'resultado': resultado})
 
 
-
+# Guardar resultado
 @csrf_exempt
 def guardar_resultado(request):
     if request.method == "POST":
@@ -83,35 +105,20 @@ def guardar_resultado(request):
             data = json.loads(request.body)
             premio_nombre = data.get("premio")
 
-            # Verificar si el premio no es "Gracias por participar"
             if premio_nombre == "Gracias por participar":
                 return JsonResponse({
                     "status": "info",
                     "message": "Sin premio, no se registra ningún ganador."
                 })
 
-            # Verificar si el cliente está en la sesión
             cliente_id = request.session.get('cliente_id')
             if not cliente_id:
                 return JsonResponse({"status": "error", "message": "Cliente no autenticado."}, status=403)
 
-            # Buscar el cliente en la base de datos
-            try:
-                cliente = Cliente.objects.get(id=cliente_id)
-            except Cliente.DoesNotExist:
-                return JsonResponse({"status": "error", "message": "Cliente no encontrado."}, status=404)
+            cliente = get_object_or_404(Cliente, id=cliente_id)
+            premio = get_object_or_404(Premio, nombre=premio_nombre, estado=True)
 
-            # Buscar el premio en la base de datos
-            try:
-                premio = Premio.objects.get(nombre=premio_nombre, estado=True)
-            except Premio.DoesNotExist:
-                return JsonResponse({"status": "error", "message": "Premio no encontrado."}, status=404)
-
-            # Crear registro en la tabla Ganador
-            Ganador.objects.create(
-                cliente=cliente,
-                premio=premio
-            )
+            Ganador.objects.create(cliente=cliente, premio=premio)
             return JsonResponse({"status": "success", "message": f"Premio registrado: {premio_nombre}"})
 
         except json.JSONDecodeError:
@@ -121,22 +128,20 @@ def guardar_resultado(request):
 
     return JsonResponse({"status": "error", "message": "Método no permitido"}, status=405)
 
-
-
-
+# Redirección al login de administrador
 def redirect_to_admin_login(request):
     return redirect('/admin/login/?next=/dashboard/')
 
-
+# Requiere superusuario
 def superuser_required(view_func):
     return user_passes_test(lambda u: u.is_superuser)(view_func)
 
+# Dashboard del administrador
 @user_passes_test(lambda u: u.is_superuser, login_url='/admin/login/')
 def dashboard(request):
     total_clientes = Cliente.objects.count()
     total_ganadores = Ganador.objects.count()
 
-    # Premio más popular
     premios_populares = (
         Ganador.objects.values('premio__nombre')
         .annotate(count=Count('premio'))
@@ -144,12 +149,10 @@ def dashboard(request):
     )
     premio_mas_popular = premios_populares[0]['premio__nombre'] if premios_populares else "N/A"
 
-    # Clientes registrados por día
     registros_por_fecha = Cliente.objects.annotate(date=TruncDay('fecha_creacion')).values('date').annotate(count=Count('id')).order_by('date')
     registros_por_fecha_labels = [str(item['date']) for item in registros_por_fecha]
     registros_por_fecha_data = [item['count'] for item in registros_por_fecha]
 
-    # Distribución de premios
     premios_distribucion = Counter(Ganador.objects.values_list('premio__nombre', flat=True))
     premios_labels = list(premios_distribucion.keys())
     premios_data = list(premios_distribucion.values())
@@ -169,24 +172,23 @@ def dashboard(request):
     }
     return render(request, 'admin/dashboard.html', context)
 
-# CRUD Views
-#CRUD premios
+# CRUD de premios
 class PremiosCRUDView(UserPassesTestMixin, ListView):
     model = Premio
     template_name = 'admin/premios_crud.html'
     
     def test_func(self):
         return self.request.user.is_superuser
-    
+
 class CrearPremioView(UserPassesTestMixin, CreateView):
     model = Premio
     fields = ['nombre', 'descripcion', 'probabilidad', 'estado']
     template_name = 'admin/crear_premio.html'
-    success_url = reverse_lazy('premios_crud')  # Redirige al CRUD de premios después de crear
+    success_url = reverse_lazy('premios_crud')
 
     def test_func(self):
-        return self.request.user.is_superuser  # Solo superusuarios pueden acceder
-    
+        return self.request.user.is_superuser
+
 class EditarPremioView(UserPassesTestMixin, UpdateView):
     model = Premio
     fields = ['nombre', 'descripcion', 'probabilidad', 'estado']
@@ -198,13 +200,13 @@ class EditarPremioView(UserPassesTestMixin, UpdateView):
 
 class EliminarPremioView(UserPassesTestMixin, DeleteView):
     model = Premio
-    template_name = 'admin/eliminar_premio.html'  # Crea esta plantilla si es necesario
+    template_name = 'admin/eliminar_premio.html'
     success_url = reverse_lazy('premios_crud')
 
     def test_func(self):
-        # Permitir acceso solo a usuarios superusuarios
         return self.request.user.is_superuser
 
+# CRUD de clientes
 class ClientesListView(UserPassesTestMixin, ListView):
     model = Cliente
     template_name = 'admin/clientes_crud.html'
@@ -213,7 +215,6 @@ class ClientesListView(UserPassesTestMixin, ListView):
     def test_func(self):
         return self.request.user.is_superuser
 
-# Crear cliente
 class CrearClienteView(UserPassesTestMixin, CreateView):
     model = Cliente
     fields = ['cedula', 'nombre', 'apellidos', 'numero_telefono', 'numero_factura']
@@ -223,17 +224,15 @@ class CrearClienteView(UserPassesTestMixin, CreateView):
     def test_func(self):
         return self.request.user.is_superuser
 
-# Editar cliente
 class EditarClienteView(UserPassesTestMixin, UpdateView):
     model = Cliente
-    fields = ['cedula', 'nombre', 'apellidos', 'numero_telefono','numero_factura']
+    fields = ['cedula', 'nombre', 'apellidos', 'numero_telefono', 'numero_factura']
     template_name = 'admin/editar_cliente.html'
     success_url = reverse_lazy('clientes_crud')
 
     def test_func(self):
         return self.request.user.is_superuser
 
-# Eliminar cliente
 class EliminarClienteView(UserPassesTestMixin, DeleteView):
     model = Cliente
     template_name = 'admin/eliminar_cliente.html'
@@ -242,13 +241,7 @@ class EliminarClienteView(UserPassesTestMixin, DeleteView):
     def test_func(self):
         return self.request.user.is_superuser
 
-
-
-#CRUD ganadores
-
-from django.urls import reverse_lazy
-
-# Listar ganadores
+# CRUD de ganadores
 class GanadoresListView(UserPassesTestMixin, ListView):
     model = Ganador
     template_name = 'admin/ganadores_crud.html'
@@ -257,7 +250,6 @@ class GanadoresListView(UserPassesTestMixin, ListView):
     def test_func(self):
         return self.request.user.is_superuser
 
-# Eliminar ganador
 class EliminarGanadorView(UserPassesTestMixin, DeleteView):
     model = Ganador
     template_name = 'admin/eliminar_ganador.html'
@@ -266,11 +258,10 @@ class EliminarGanadorView(UserPassesTestMixin, DeleteView):
     def test_func(self):
         return self.request.user.is_superuser
 
-
 @require_POST
 @csrf_exempt
 def toggle_estado_premio(request, pk):
     premio = get_object_or_404(Premio, pk=pk)
-    premio.estado = not premio.estado  # Cambia el estado del premio
+    premio.estado = not premio.estado
     premio.save()
     return JsonResponse({'status': 'success'})
